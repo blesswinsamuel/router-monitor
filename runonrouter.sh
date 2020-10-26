3c6b6ceb9eb6b736423dc7e157a79827a96795e2
Blesswin Samuel <blesswinsamuel@gmail.com>
2020-10-27 00:43:07 +0530

    implement dhcp hostname lookup table

--- runonrouter.sh
+++ runonrouter.sh
@@ -5,4 +5,4 @@ docker run --rm --entrypoint cat blesswinsamuel/network_traffic_exporter:latest 
 
 ssh -p 2244 pi@router.home.local "sudo killall -9 /tmp/network_traffic_exporter || true"
 scp -P 2244 /tmp/network_traffic_exporter pi@router.home.local:/tmp/network_traffic_exporter
-ssh -p 2244 pi@router.home.local "sudo -E /tmp/network_traffic_exporter -listen 0.0.0.0:9155 -l4 -interface lan -bpf 'src net 192.168.1.0/24 or dst net 192.168.1.0/24'"
+ssh -p 2244 pi@router.home.local "sudo -E /tmp/network_traffic_exporter -listen 0.0.0.0:9155 -interface lan -bpf 'src net 192.168.1.0/24 or dst net 192.168.1.0/24'"
