be847976d2034956f1a5eef41081298fc648ed84
Blesswin Samuel <blesswinsamuel@gmail.com>
2024-01-29 16:26:47 +0530

    add internet pings with more ips, make ddns async await

--- src/internet_check.rs
+++ src/internet_check.rs
@@ -1,32 +1,38 @@
-use std::{
-    net::{SocketAddr, TcpStream},
-    time::{Duration, Instant},
-};
+use std::time::{Duration, Instant};
 
 use prometheus_client::{
-    metrics::{gauge::Gauge, histogram::Histogram},
+    encoding::EncodeLabelSet,
+    metrics::{family::Family, gauge::Gauge, histogram::Histogram},
     registry::Registry,
 };
+use tokio::net::TcpStream;
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelSet)]
+struct InternetUpLabels {
+    addr: String,
+}
 
 pub struct InternetCheck {
-    connection_duration: Histogram,
-    connection_is_up: Gauge,
+    connection_duration: Family<InternetUpLabels, Histogram>,
+    connection_is_up: Family<InternetUpLabels, Gauge>,
 }
 
 impl InternetCheck {
     pub fn new() -> Self {
         Self {
-            connection_duration: Histogram::new(vec![0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0].into_iter()),
-            connection_is_up: Gauge::default(),
+            connection_duration: Family::new_with_constructor(|| {
+                Histogram::new(vec![0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0].into_iter())
+            }),
+            connection_is_up: Default::default(),
         }
     }
 
-    pub fn start(&self) {
+    pub async fn start(&self) {
         let ticker = crossbeam::channel::tick(Duration::from_secs(10));
-        self.is_internet_connection_up();
+        self.is_internet_connection_up().await;
         loop {
             let _ = ticker.recv();
-            self.is_internet_connection_up();
+            self.is_internet_connection_up().await;
         }
     }
 
@@ -39,18 +45,28 @@ impl InternetCheck {
         registry.register("router_monitor_internet_connection_is_up", "Whether internet connection is up", self.connection_is_up.clone());
     }
 
-    fn is_internet_connection_up(&self) {
+    async fn is_internet_connection_up(&self) {
         let start_time = Instant::now();
-        match TcpStream::connect_timeout(&"1.1.1.1:443".parse::<SocketAddr>().unwrap(), Duration::from_secs(5)) {
-            Ok(_) => {
-                // println!("Connected to the server!");
-                self.connection_is_up.set(1);
-                self.connection_duration.observe((Instant::now() - start_time).as_secs_f64());
-            }
-            Err(err) => {
-                println!("Internet is down: {}", err);
-                self.connection_is_up.set(0);
-                self.connection_duration.observe((Instant::now() - start_time).as_secs_f64());
+        let addrs = [
+            // https://www.reddit.com/r/sysadmin/comments/uo36rh/10_to_20_safe_high_performance_ip_addresses_that/
+            "1.1.1.1:53",
+            "64.6.64.6:53",
+            "8.8.8.8:53",
+            "208.67.222.222:53",
+            "9.9.9.9:53",
+        ];
+        for addr in addrs.iter() {
+            let labels = &InternetUpLabels { addr: addr.to_string() };
+            match tokio::time::timeout(Duration::from_secs(2), TcpStream::connect(addr)).await {
+                Ok(_) => {
+                    self.connection_is_up.get_or_create(labels).set(1);
+                    self.connection_duration.get_or_create(labels).observe((Instant::now() - start_time).as_secs_f64());
+                }
+                Err(err) => {
+                    log::error!("Failed to connect to {}: {}", addr, err);
+                    self.connection_is_up.get_or_create(labels).set(0);
+                    self.connection_duration.get_or_create(labels).observe((Instant::now() - start_time).as_secs_f64());
+                }
             }
         }
     }
