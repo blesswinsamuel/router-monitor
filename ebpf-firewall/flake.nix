4a2e8200d5db0a0da4dbc161ab5745c00054b876
Blesswin Samuel <blesswinsamuel@gmail.com>
2024-05-07 04:41:00 +0530

    nix flake wip

--- ebpf-firewall/flake.nix
+++ ebpf-firewall/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A flake for building Hello World";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
@@ -9,28 +9,47 @@
     # https://ryantm.github.io/nixpkgs/stdenv/stdenv/
     flake-utils.lib.eachDefaultSystem (system: {
       packages = rec {
-        router-monitor =
+        ebpf-firewall =
           let
             pkgs = import nixpkgs { inherit system; };
           in
-          pkgs.stdenv.mkDerivation {
-            name = "router-monitor";
-            src = self;
-            buildInputs = [
-              pkgs.gcc
-            ];
-            buildPhase = "gcc -o router-monitor ./hello.c";
-            installPhase = "mkdir -p $out/bin; install -t $out/bin router-monitor";
-            # name = "router-monitor";
-            # src = self;
-            # buildInputs = [
-            #   pkgs.gcc
-            #   pkgs.go
-            # ];
-            # buildPhase = "go generate";
-            # installPhase = "mkdir -p $out/bin; install -t $out/bin counter";
-          };
-        default = router-monitor;
+          # pkgs.stdenv.mkDerivation {
+            #   name = "ebpf-firewall";
+            #   src = self;
+            #   buildInputs = [
+            #     pkgs.gcc
+            #     pkgs.go
+            #     pkgs.dig
+            #   ];
+            #   # buildPhase = "gcc -o ebpf-firewall ./hello.c";
+            #   # installPhase = "mkdir -p $out/bin; install -t $out/bin ebpf-firewall";
+            #   # name = "ebpf-firewall";
+            #   # src = self;
+            #   buildPhase = ''
+            #     export HOME=$(pwd)
+            #     dig google.com
+            #     CGO_ENABLED=0 go mod download
+            #     go generate ./...
+            #     go build -o ebpf-firewall
+            #   '';
+            #   installPhase = "mkdir -p $out/bin; install -t $out/bin ebpf-firewall";
+            # };
+          pkgs.buildGoModule
+            {
+              pname = "ebpf-firewall";
+              version = "0.0.1";
+
+              src = ./.;
+
+              modPostBuild = ''
+                go generate ./...
+              '';
+
+              # proxyVendor = true;
+              # vendorHash = "";
+              vendorHash = "sha256-34+UzVtcH0DohkFNM9dpsAdBh8tCoS7WksiN01j1tng=";
+            };
+        default = ebpf-firewall;
       };
     });
 }
