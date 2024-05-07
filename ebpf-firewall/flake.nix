c0cfee2bae7009900f47d8af7f464f46503201ef
Blesswin Samuel <blesswinsamuel@gmail.com>
2024-05-07 05:40:48 +0530

    try out nix remote build

--- ebpf-firewall/flake.nix
+++ ebpf-firewall/flake.nix
@@ -1,55 +1,29 @@
 {
-  description = "A flake for building Hello World";
+  description = "A basic gomod2nix flake";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.gomod2nix.url = "github:nix-community/gomod2nix";
+  inputs.gomod2nix.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.gomod2nix.inputs.flake-utils.follows = "flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, gomod2nix }:
+    (flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
 
-    # https://ryantm.github.io/nixpkgs/stdenv/stdenv/
-    flake-utils.lib.eachDefaultSystem (system: {
-      packages = rec {
-        ebpf-firewall =
-          let
-            pkgs = import nixpkgs { inherit system; };
-          in
-          # pkgs.stdenv.mkDerivation {
-            #   name = "ebpf-firewall";
-            #   src = self;
-            #   buildInputs = [
-            #     pkgs.gcc
-            #     pkgs.go
-            #     pkgs.dig
-            #   ];
-            #   # buildPhase = "gcc -o ebpf-firewall ./hello.c";
-            #   # installPhase = "mkdir -p $out/bin; install -t $out/bin ebpf-firewall";
-            #   # name = "ebpf-firewall";
-            #   # src = self;
-            #   buildPhase = ''
-            #     export HOME=$(pwd)
-            #     dig google.com
-            #     CGO_ENABLED=0 go mod download
-            #     go generate ./...
-            #     go build -o ebpf-firewall
-            #   '';
-            #   installPhase = "mkdir -p $out/bin; install -t $out/bin ebpf-firewall";
-            # };
-          pkgs.buildGoModule
-            {
-              pname = "ebpf-firewall";
-              version = "0.0.1";
-
-              src = ./.;
-
-              modPostBuild = ''
-                go generate ./...
-              '';
-
-              # proxyVendor = true;
-              # vendorHash = "";
-              vendorHash = "sha256-34+UzVtcH0DohkFNM9dpsAdBh8tCoS7WksiN01j1tng=";
-            };
-        default = ebpf-firewall;
-      };
-    });
+          # The current default sdk for macOS fails to compile go projects, so we use a newer one for now.
+          # This has no effect on other platforms.
+          callPackage = pkgs.darwin.apple_sdk_11_0.callPackage or pkgs.callPackage;
+        in
+        {
+          packages.default = callPackage ./. {
+            inherit (gomod2nix.legacyPackages.${system}) buildGoApplication;
+          };
+          devShells.default = callPackage ./shell.nix {
+            inherit (gomod2nix.legacyPackages.${system}) mkGoEnv gomod2nix;
+          };
+        })
+    );
 }
