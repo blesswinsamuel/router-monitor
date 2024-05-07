2b93ab1553f3e88b0ab46b9339ee4ffc1e4d3017
Blesswin Samuel <blesswinsamuel@gmail.com>
2024-05-08 02:52:13 +0530

    remove router-monitor rust code, move ebpf-firewall to root

--- flake.nix
+++ flake.nix
@@ -1,39 +1,29 @@
 {
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    fenix = { url = "github:nix-community/fenix"; inputs.nixpkgs.follows = "nixpkgs"; };
-    naersk = { url = "github:nix-community/naersk"; inputs.nixpkgs.follows = "nixpkgs"; };
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  description = "A basic gomod2nix flake";
 
-  outputs = { self, fenix, flake-utils, naersk, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      packages.default =
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.gomod2nix.url = "github:nix-community/gomod2nix";
+  inputs.gomod2nix.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.gomod2nix.inputs.flake-utils.follows = "flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, gomod2nix }:
+    (flake-utils.lib.eachDefaultSystem
+      (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          target = "aarch64-unknown-linux-gnu";
-          toolchain = with fenix.packages.${system}; combine [
-            minimal.cargo
-            minimal.rustc
-            targets.${target}.latest.rust-std
-          ];
+
+          # The current default sdk for macOS fails to compile go projects, so we use a newer one for now.
+          # This has no effect on other platforms.
+          callPackage = pkgs.darwin.apple_sdk_11_0.callPackage or pkgs.callPackage;
         in
-
-        (naersk.lib.${system}.override {
-          cargo = toolchain;
-          rustc = toolchain;
-        }).buildPackage {
-          src = ./.;
-
-          nativeBuildInputs = [ pkgs.pkg-config ];
-          # buildInputs = [ pkgs.openssl ];
-
-          CARGO_BUILD_TARGET = target;
-          CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER =
-            let
-              inherit (pkgs.pkgsCross.aarch64-multiplatform.stdenv) cc;
-            in
-            "${cc}/bin/${cc.targetPrefix}cc";
-        };
-    });
+        {
+          packages.default = callPackage ./. {
+            inherit (gomod2nix.legacyPackages.${system}) buildGoApplication;
+          };
+          devShells.default = callPackage ./shell.nix {
+            inherit (gomod2nix.legacyPackages.${system}) mkGoEnv gomod2nix;
+          };
+        })
+    );
 }
