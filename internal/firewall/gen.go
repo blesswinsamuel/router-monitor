e49439d75aac56c07fbdfc7be37e899cccc52be6
Blesswin Samuel <blesswinsamuel@gmail.com>
2024-05-08 04:34:29 +0530

    amd64 build

--- internal/firewall/gen.go
+++ internal/firewall/gen.go
@@ -1,3 +1,3 @@
 package firewall
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target arm64 ebpfFirewall ebpf_firewall.c
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target amd64 ebpfFirewall ebpf_firewall.c
