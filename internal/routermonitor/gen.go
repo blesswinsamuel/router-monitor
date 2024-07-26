be5387e41889a6f9a4b25095f91e56994f573a81
Blesswin Samuel <blesswinsamuel@gmail.com>
2024-07-26 16:14:59 +0530

    build for amd64

--- internal/routermonitor/gen.go
+++ internal/routermonitor/gen.go
@@ -1,3 +1,3 @@
 package routermonitor
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target arm64 ebpfCollector ebpf-collector.c
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target amd64 ebpfCollector ebpf-collector.c
